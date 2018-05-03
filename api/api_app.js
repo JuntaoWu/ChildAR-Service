991ae63056fb8baa128b8dd1661cc3c902f7b7ec
chenlunsen <563575103@qq.com>
2018-05-03 11:37:54 +0800

    new

---
+++ api/api_app.js
@@ -0,0 +1,141 @@
+
+var fs = require('fs');
+
+module.exports = function(){
+	function getVersionObj(callback) {
+		callback = callback || function(){};
+		fs.readFile('./version.json', 'utf8', function(err, data){
+			if(!data) {
+				var data = {
+					historyVersion: [],
+				}
+			} else {
+				data = JSON.parse(data);
+			}
+		});
+	}
+	function setVersionObj(obj, callback) {
+		
+	}
+	
+	var updateVersion = function(req, res) {
+		if(!req.body.version) {
+			res.sendErr('err');
+		}
+		var obj = {
+				version: req.body.version,
+				description: req.body.description || '',
+				path: req.body.path || '',
+				os: req.body.os || '',
+				updatetime: Date.parse(new Date())
+			},
+			versionNums = req.body.version.split('.');
+		
+		fs.readFile('./version.json', 'utf8', function(err, data){
+			if(!data) {
+				var data = {
+					historyVersion: [],
+				}
+			} else {
+				data = JSON.parse(data);
+			}
+			var len = data.historyVersion.length,
+				shouldAdd = true;
+			if(!len){
+				data.historyVersion.push(obj);
+			} else {
+				for(var i = len - 1; i >= 0; i--){
+					if(data.historyVersion[i].version == obj.version){
+						//更新版本信息
+						data.historyVersion[i].description = obj.description || data.historyVersion[i].description;
+						data.historyVersion[i].path = obj.path || data.historyVersion[i].path;
+						data.historyVersion[i].os = obj.os || data.historyVersion[i].os;
+						data.historyVersion[i].updatetime = Date.parse(new Date());
+						shouldAdd = false;
+						break;
+					}
+				}
+				if(shouldAdd){
+					//插入版本信息
+					for(var i = len; i > 0; i--){
+						versN = data.historyVersion[i-1].version.split('.');
+						if(versN[0] > versionNums[0]){
+							data.historyVersion[i] = data.historyVersion[i-1];
+						} else if (versN[1] > versionNums[1]){
+							data.historyVersion[i] = data.historyVersion[i-1];
+						} else if (versN[2] > versionNums[2]) {
+							data.historyVersion[i] = data.historyVersion[i-1];
+						} else {
+							data.historyVersion[i] = obj;
+							break;
+						}
+					}
+				}
+			}
+			fs.writeFile('./version.json', JSON.stringify(data),function(err){
+				if(err) {
+					res.sendErr('fail');
+				} else {
+					res.send('success');
+				}
+			});
+		});
+	}
+	
+	var getVersionList = function(req, res) {
+		console.log(res);
+		fs.readFile('./version.json', 'utf8', function(err, data){
+			console.log(data);  
+			if(err) {
+				res.sendErr(err);
+			}
+			if(!data){
+				res.send([]);
+			} else {
+				data = JSON.parse(data).historyVersion;
+				res.send(data);
+			}
+		});
+	}
+	
+	var getVersion = function(req, res) {
+		var version = req.body.version;
+		fs.readFile('./version.json', 'utf8', function(err, data){
+			console.log(data);  
+			if(err) {
+				res.sendErr(err);
+			}
+			data = JSON.parse(data).historyVersion;
+			for(var i = 0; i < data.length; i++){
+				if(data[i].version == version) {
+					res.send(data[i]);
+				}
+			}
+			res.send(null);
+		});
+	}
+	
+	var getLastVersion = function(req, res) {
+		
+		fs.readFile('./version.json', 'utf8', function(err, data){
+			console.log(data);  
+			if(err) {
+				res.sendErr(err);
+			}
+			data = JSON.parse(data).historyVersion;
+			if(data.length) {
+				res.send(data[data.length - 1]);
+			} else {
+				res.send(null);
+			}
+		});
+	}
+
+	
+	return {
+		getVersionList: getVersionList,
+		getVersion: getVersion,
+		updateVersion: updateVersion,
+		getLastVersion: getLastVersion,
+	}
+}()
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var UglifyJS = require('uglify-js');
+var doCompressJS = false;
+var isStopWebSite = false;
 
 var router = express.Router(); 
 var app = express();
@@ -15,82 +18,110 @@ app.get('/', function (req, res) {
 	res.send('helloworld');
 })
 
-var api_app = {
-	updateVersion: function(req, res) {
-		var obj = {
-			version: req.body.version || '',
-			description: req.body.description || '',
-			path: req.body.path || '',
-			os: req.body.os || '',
-			updatetime: req.body.time || ''
+//处理静态文件
+router.get(/[^\#\?]\.(css|jpg|jpeg|gif|png|ttf|js|icon|txt|json|xls|xlsx)$/i, function(req, res) {
+	
+	var str= "";
+	var options = {
+		root: __dirname + '/public/',
+		dotfiles: 'deny'
+	};
+	
+	var matchArr = (req.url + "").match(/[^\#\?]\.(css|jpg|jpeg|gif|png|ttf|js|icon|txt|json|xls|xlsx)$/i);
+	if(matchArr && matchArr.length){
+		var ext = matchArr[1];
+	}
+	else{
+		res.status(404).end("<h1>404 ERROR, File Not Found!</h1>");
+		return;
+	}
+	//var ext = (req.url + "").match(/[^\#\?]\.(css|jpg|gif|png|ttf|js|icon|txt|json)$/i)[1];
+	var loc =  __dirname + '/public/' + decodeURI(req.url);
+	
+	//压缩混淆js，并缓存
+	if(doCompressJS && ext.toLowerCase() == "js" && !(req.url + "").match(/min\.js$/i)){
+		
+		var reqETag = req.headers["if-none-match"];
+		if(reqETag && jsContentHash[req.url] && jsContentHash[req.url].ETag && reqETag == jsContentHash[req.url].ETag){
+			res.status(304).end("");
+			return;
+		} 
+		else if(jsContentHash[req.url]){
+			res.type("application/javascript").set("ETag",jsContentHash[req.url].ETag).send(jsContentHash[req.url].code);
 		}
-		console.log(req.body)
-		console.log(obj)
-		if(!req.body.version) {
-			res.sendErr('err');
-		}
-		fs.readFile('./version.json', 'utf8', function(err, data){
-			if(!data) {
-				var data = {
-					historyVersion: [],
+		else{
+			
+			fs.readFile(loc, function (err, data) {
+				if(err){
+					res.status(400).end("<h1>404 ERROR, File Not Foundf!</h1>");
 				}
-			} else {
-				data = JSON.parse(data);
-			}
-			var len = data.historyVersion.length,
-				shouldAdd = true;
-			for(var i = 0; i < len; i++){
-				if(data.historyVersion[i].version == obj.version){
-					//更新版本信息
-					data.historyVersion[i].description = obj.description || data.historyVersion[i].description;
-					data.historyVersion[i].path = obj.path || data.historyVersion[i].path;
-					data.historyVersion[i].os = obj.os || data.historyVersion[i].os;
-					data.historyVersion[i].updatetime = obj.updatetime || data.historyVersion[i].updatetime;
-					shouldAdd = false;
-				}
-			}
-			if(shouldAdd){
-				data.historyVersion.push(obj);
-			}
-			fs.writeFile('./version.json', JSON.stringify(data),function(err){
-				if(err) {
-					res.send('fail');
-					console.log('写文件操作失败');
-				} else {
-					res.send('sussce');
-					console.log('写文件操作成功');
+				else{
+					console.time("!!compress JS "+req.url+", time used:");
+					var compressJS = UglifyJS.minify(data+"", {fromString: true});
+					console.timeEnd("!!compress JS "+req.url+", time used:");
+					var etagStr = Math.floor(Math.random()*100000)+"_"+Math.floor(Math.random()*100000)+"_"+Math.floor(Math.random()*100000)
+					jsContentHash[req.url] = {code:compressJS.code, ETag: etagStr};
+					res.type("application/javascript").set("ETag",etagStr).send(compressJS.code);
 				}
 			});
-		});
-	},
-	
-	getList: function(req, res) {
-		fs.readFile('./version.json', 'utf8', function(err, data){
-			console.log(data);  
-			if(!data){
-				res.send([]);
-			} else {
-				data = JSON.parse(data).historyVersion;
-				res.send(data);
-			}
-		});
-	},
-	
-	getVersion: function(req, res) {
-		var version = req.body.version;
-		fs.readFile('./version.json', 'utf8', function(err, data){
-			console.log(data);  
-			data = JSON.parse(data).historyVersion;
-			for(var i = 0; i < data.length; i++){
-				if(data[i].version == version) {
-					res.send(data[i]);
+		}
+		
+	}
+	else{
+		
+		var sendFileLoc = decodeURI(req.url);
+		res.sendFile(sendFileLoc, options,  function (err) {
+				if (err) {
+					if(err.status == "404"){
+						res.status(err.status).end("<h1>404 ERROR, File Not Found!</h1>");
+						return;
+					}
+					else if(err.status == "500"){
+						res.status(err.status).end("<h1>500 ERROR, Server Error!</h1>");
+						return;
+					}
+					res.status(err.status).end();
+				}
+				else {
 				}
 			}
-			res.send(null);
-		});
-	},
-}
+		)
+	}
+	
+});
 
+//由于html可以通过?传递参数，所以进行单独处理
+router.get(/([^\#\?]\.html|[^\#\?]\.html[\?][\d\D]*)$/i, function(req, res) {
+	if(isStopWebSite){
+		res.send("<h1>正在维护中,请稍后再试。</h1>");
+		return;
+	}
+	var str= "";
+	
+	var options = {
+		root: __dirname + '/public/',
+		dotfiles: 'deny'
+	};
+	var location_str = req.url.replace(/\?[\d\D]*$/,"");
+	res.sendFile(location_str, options,  function (err) {
+			if (err) {
+				if(err.status == "404"){
+					res.status(err.status).end("<h1>404 ERROR, File Not Found!</h1>");
+					return;
+				}
+				else if(err.status == "500"){
+					res.status(err.status).end("<h1>500 ERROR, Server Error!</h1>");
+					return;
+				}
+				res.status(err.status).end();
+			}
+			else {
+			}
+		}
+	)
+});
+
+var api_app = require('./api/api_app.js');
 var interfacePostAndGetProc = function(req, res){
 	var matrrr = req.url.match(/^\/api\/([^\?]+)/);
 	var jsonpCallBack = "";
